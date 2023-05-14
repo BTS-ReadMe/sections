0393356ee10c13742e4ffb906d745cdb6f45a9ea
jinyeong-afk <xoeh7002@naver.com>
2023-05-14 23:55:51 +0900

    [UPDATE] #BTS-101 : schedule table 설정 추가

--- src/main/java/com/readme/sections/model/Schedule.java
+++ src/main/java/com/readme/sections/model/Schedule.java
@@ -1,30 +1,35 @@
 package com.readme.sections.model;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Table(name = "schedule")
 public class Schedule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     Long id;
+    @Column(name = "name", nullable = false)
     String name;
+    @Column(name = "start_date", nullable = false)
     LocalDateTime startDate;
+    @Column(name = "end_date", nullable = false)
     LocalDateTime endDate;
 }
