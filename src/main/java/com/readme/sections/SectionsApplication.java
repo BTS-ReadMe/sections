fca027641aa902977b169e605643e7990e2841f2
jinyeong-afk <xoeh7002@naver.com>
2023-05-18 15:54:14 +0900

    [UPDATE] #BTS-123 : EurekClient 활성화

--- src/main/java/com/readme/sections/SectionsApplication.java
+++ src/main/java/com/readme/sections/SectionsApplication.java
@@ -3,7 +3,7 @@ package com.readme.sections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.readme.sections")
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableJpaAuditing
 @EnableScheduling
 public class SectionsApplication {
